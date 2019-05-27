3fe9af301c4a2c2f36fc06d9385cb44c19308e12
João Victor Silva <39493542+joovictsousa@users.noreply.github.com>
2019-05-27 13:48:48 -0400

    Update DFS.cpp

--- Code Book/DFS.cpp
+++ Code Book/DFS.cpp
@@ -1,23 +1,35 @@
-#include <bits/stdc++.h>
-#define _ ios_base::sync_with_stdio(0);
-#define MAXN 1123
+#include<bits/stdc++.h>
+#define N 200023
 #define vi vector<int>
-#define vvi vector<vi>
+#define _ ios_base::sync_with_stdio(0);
+#define pb push_back
 
 using namespace std;
 
-vi adj[MAXN];
-int seen[MAXN];
+int n, ini;
+int vis[N];
+vi adj[N];
 
-void DFS(int u){
-    seen[u]=1;
-    for(int v : adj[u]){
-        if(!seen[v]) dfs(v);
-    }
+int dfs(int u){
+	vis[u] = 1;
+	int dist=0; //Máxima distância
+	for(int v : adj[u]){
+		if(!vis[v])
+			dist=max(dfs(v) + 1, dis);
+	}
+	return dis;
 }
 
 int main(){_
-    //Códigos;
-    
-    return 0;
+	int u, v;
+	cin >> n >> ini;
+	for(int i = 1; i < n; ++i){
+		cin >> u >> v;
+		adj[u].pb(v);
+		adj[v].pb(u);
+	}
+	
+	int ans = dfs(ini) + 1; //o é o ponto inicial da DFS
+	cout << ans << endl;
+	return 0;
 }
